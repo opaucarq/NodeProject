7a867786674f1f010343fcaf784fd6f4419cdafc
Oliver <opaucarq@uni.pe>
2023-08-04 10:33:42 -0500

    redirecting requests

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
   const url = req.url
+  const method = req.method
   
   if(url === '/'){
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
-    res.write('<body><form action="/message" method="post"><input type="text" name="message"/> <button type="submit"></button></form></body>')
+    res.write('<body><form action="/message" method="post"><input type="text" name="message"/> <button type="submit">Send Message</button></form></body>')
     res.write('</html>')  
     return res.end()
+  }
+
+  if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', 'Dummy')
+    res.statusCode = 302;
+    res.setHeader('Location','/')
+    return res.end();
   }
 
   res.setHeader('Content-Type','text/html');
