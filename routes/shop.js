03a8eeeb863bf6190ded7989d80ebc5cacc227c3
Oliver <opaucarq@uni.pe>
2023-08-07 10:12:09 -0500

    styling pages

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const rootDir = require('../utils/path')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-  res.sendFile(path.join(rootDir,'views','add-product.html'));
+  res.sendFile(path.join(rootDir,'views','shop.html'));
 })
 
 module.exports = router
